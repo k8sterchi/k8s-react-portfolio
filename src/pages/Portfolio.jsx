3971489211e0733f87f4fbb2f0ebe9328478b30d
k8 <katesterchi@gmail.com>
2023-11-12 11:30:13 -0600

    began fleshing out the portfolio section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,15 +9,15 @@ const Portfolio = () => {
         {/* Project 1 */}
         <ProjectCard
           title="Project 1"
-          imageSrc="path/to/project1-image.jpg"
-          deployLink="https://example.com/project1"
-          githubLink="https://github.com/yourusername/project1"
+          imageSrc="./src/images/Project1_k8s.png"
+          deployLink="https://k8sterchi.github.io/K8-Portfolio/"
+          githubLink="https://github.com/k8sterchi/K8-Portfolio"
         />
 
         {/* Project 2 */}
         <ProjectCard
           title="Project 2"
-          imageSrc="path/to/project2-image.jpg"
+          imageSrc="./src/images/Project2_k8s.png"
           deployLink="https://example.com/project2"
           githubLink="https://github.com/yourusername/project2"
         />
@@ -25,7 +25,7 @@ const Portfolio = () => {
          {/* Project 3 */}
          <ProjectCard
           title="Project 3"
-          imageSrc="path/to/project3-image.jpg"
+          imageSrc="./src/images/Project3_k8s.png"
           deployLink="https://example.com/project3"
           githubLink="https://github.com/yourusername/project3"
         />
@@ -33,7 +33,7 @@ const Portfolio = () => {
          {/* Project 4 */}
          <ProjectCard
           title="Project 4"
-          imageSrc="path/to/project4-image.jpg"
+          imageSrc="./src/images/Project4_k8s.png"
           deployLink="https://example.com/project4"
           githubLink="https://github.com/yourusername/project4"
         />
@@ -41,7 +41,7 @@ const Portfolio = () => {
          {/* Project 5 */}
          <ProjectCard
           title="Project 5"
-          imageSrc="path/to/project5-image.jpg"
+          imageSrc="./src/images/Project5_k8s.png"
           deployLink="https://example.com/project5"
           githubLink="https://github.com/yourusername/project5"
         />
@@ -49,7 +49,7 @@ const Portfolio = () => {
          {/* Project 6 */}
          <ProjectCard
           title="Project 6"
-          imageSrc="path/to/project6-image.jpg"
+          imageSrc="./src/images/Project6_k8s.png"
           deployLink="https://example.com/project6"
           githubLink="https://github.com/yourusername/project6"
         />
