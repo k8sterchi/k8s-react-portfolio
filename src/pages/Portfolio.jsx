c5d03c10b761038d62a295b52f294b48ce8b733c
k8 <katesterchi@gmail.com>
2023-11-12 09:03:52 -0600

    set up initial files with boiler plate code, successfully rendering basic pages

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,10 @@
+export default function Portfolio() {
+    return (
+      <div>
+        <h1>Portfolio</h1>
+        <p>
+         ADD PORTFOLIO IMAGES and LINKS ETC HERE
+        </p>
+      </div>
+    );
+  }
