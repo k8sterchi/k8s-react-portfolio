3c474c609de959718da76b88a0bfdb970c8cc607
k8 <katesterchi@gmail.com>
2023-11-12 10:17:46 -0600

    added basic resume, created form in contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,74 @@
-export default function Contact() {
-    return (
-      <div>
-        <h1>Contact</h1>
-        <p>
-         email me @ my name @ email dot com 
-        </p>
-      </div>
-    );
-  }
+// Contact.jsx
+
+import React, { useState } from 'react';
+import '../App.css'; 
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your logic to handle form submission (e.g., send data to a server, display a success message, etc.)
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <div className="contact-container">
+      <h1>Contact</h1>
+
+      <form onSubmit={handleSubmit} className="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
+
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,34 @@
 export default function Resume() {
     return (
-      <div>
-        <h1>Resume</h1>
-        <p>
-         Education experience etc... 
-        </p>
-      </div>
-    );
-  }
+        <div>
+          {/* <h1>Resume</h1> */}
+    
+          <section>
+            <h2>Education</h2>
+            <div>
+              <h3>Funen Art Academy, Odense, Denmark</h3>
+              <p>MFA</p>
+              <p>Painting</p>
+              <p>Graduation Year: 2017</p>
+            </div>
+            {/* Add more education entries if needed */}
+          </section>
+    
+          <section>
+            <h2>Experience</h2>
+            <div>
+              <h3>Judd Foundation</h3>
+              <p>Artist/Guide</p>
+              <p>Marfa, Texas</p>
+              <p>2021 - present</p>
+            </div>
+            <div>
+              <h3>Marfa Food Pantry</h3>
+              <p>Coordinator</p>
+              <p>Marfa, Texas</p>
+              <p>2020 - 2022</p>
+            </div>
+          </section>
+        </div>
+      );
+    };
